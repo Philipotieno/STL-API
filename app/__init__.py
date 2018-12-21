60b8351406f5c4fc5cad52402ef80114b76590bb
philipotieno <tmitchellb007@gmail.com>
2018-12-21 12:45:40 +0300

    [ch-#162680095] Add a create_app func that creates anew flask abject

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,16 @@
-requirements
+from flask import Flask
+
+from config import app_config
+
+
+def create_app(env_name):
+	""" Cretae app """
+
+	#app ininitialization
+
+	app = Flask(__name__)
+	app.config.from_object(app_config[env_name])
+	app.register_blueprint(v1_user, url_prefix="/signup")
+
+
+	return app
