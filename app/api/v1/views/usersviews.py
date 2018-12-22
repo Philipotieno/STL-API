b0988494e83598f4c4092408277dc4fc11ebfb42
philipotieno <tmitchellb007@gmail.com>
2018-12-22 10:38:31 +0300

    [ft-#162680095] username validation

--- app/api/v1/views/usersviews.py
+++ app/api/v1/views/usersviews.py
@@ -2,10 +2,11 @@ from flask import Blueprint, jsonify, request
 from app.api.v1.models.usersmodels import UserModel
 
 import os
+import re
 
 v1_user = Blueprint('UserModel', __name__)
 
-user_instance = UserModel() #User class instance
+#user_instance = UserModel() #User class instance
 
 @v1_user.route('/register',methods=['POST'])
 def register_user():
@@ -15,5 +16,22 @@ def register_user():
 	username = data["username"]
 	email = data["email"]
 	password = data["password"]
+
+	emai_format = re.compile(
+		r"(^[a-zA-Z0-9_.-]+@[a-zA-Z-]+\.[a-zA-Z-]+$)")
+	name_format = re.compile(r"(^[A-Za-z]+$)")
+
+	if not (re.match(name_format, fname)):
+		return jsonify({'message' : 'Invalid first name'}), 400
+
+	if not (re.match(name_format, lname)):
+		return jsonify({'message' : 'Invalid last name'}), 400
+
+	if not (re.match(name_format, username)):
+		return jsonify({'message' : 'Invalid username'}), 400
+
+	if len(fname) or len(lname) < 4:
+		return jsonify({"message" : "length of name or username is too short"}), 400
+
 
 	return jsonify({'message' : 'user registration succesfull'})
